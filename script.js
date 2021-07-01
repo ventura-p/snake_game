5f3c749dd5cc6e98afc6831344155192d7cf04b3
ventura-p <vinicius.v.pereira@ufv.br>
2021-07-01 17:20:21 -0300

    jogo final

--- script.js
+++ script.js
@@ -25,11 +25,33 @@ function criarcobrinha() {
 
 }
 
+function drawFood(){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+}
+
+document.addEventListener('keydown', update)
+
+function update (event){
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up"
+    if(event.keyCode == 39 && direction != "left") direction = "right"
+    if(event.keyCode == 40 && direction != "up") direction = "down"
+}
+
 function iniciarJogo(){
-    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
-    if(snake[0].y < 0 * box && direction == "left") snake[0].x = 16 * box;
-    if(snake[0].x > 15 * box && direction == "up") snake[0].y = 0;
-    if(snake[0].y < 0 * box && direction == "down") snake[0].y = 16 * box;
+
+    if(snake[0].x > 15*box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
+    if(snake[0].y > 15*box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
+
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[1].x && snake[0].y == snake[1].y){
+            clearInterval(jogo);
+            alert("Game Over :(");
+        }
+    }
 
     criarBG();
     criarcobrinha();
@@ -57,21 +79,7 @@ function iniciarJogo(){
     }
 
     snake.unshift(newHead)
-}
 
-function drawFood(){
-    context.fillStyle = "red";
-    context.fillRect(food.x, food.y, box, box);
-}
-
-document.addEventListener('keydown', update)
-
-function update (event){
-    if(event.keyCode == 37 && direction != "right") direction = "left";
-    if(event.keyCode == 38 && direction != "down") direction = "up"
-    if(event.keyCode == 39 && direction != "left") direction = "right"
-    if(event.keyCode == 40 && direction != "up") direction = "down"
 }
 
 let jogo = setInterval(iniciarJogo, 100);
-
